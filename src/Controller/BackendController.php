148c5301cad8872d4b79ab0dc3ea13c31cd27d64
ShyMad <imad.belarabi@gmail.com>
2018-06-06 21:48:25 +0200

    changes in backendController and views

--- src/Controller/BackendController.php
+++ src/Controller/BackendController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Project;
+use App\Form\ProjectType;
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class BackendController extends Controller
 {
@@ -14,15 +20,84 @@ class BackendController extends Controller
     {
         return $this->render('backend/index.html.twig', [
             'controller_name' => 'BackendController',
+            'tag' => 'My Dashboard',
         ]);
     }
     /**
-     * @Route("/backend/formation", name="formation")
+     * @Route("backend/project/", name="project_index", methods="GET")
      */
-    public function formation()
+    public function p_index(ProjectRepository $projectRepository): Response
     {
-        return $this->render('backend/formation.html.twig',[
-            'controller_name' => 'BackendController',
+        return $this->render('backend/project/index.html.twig',[
+        'projects' => $projectRepository->findAll(),
+        'tag' => 'Project',
         ]);
+    }
+
+    /**
+     * @Route("backend/project/new", name="project_new", methods="GET|POST")
+     */
+    public function p_new(Request $request): Response
+    {
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($project);
+            $em->flush();
+
+            return $this->redirectToRoute('project_index');
+        }
+
+        return $this->render('backend/project/new.html.twig', [
+            'project' => $project,
+            'tag' => 'new project',
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/project/{id}", name="project_show", methods="GET")
+     */
+    public function p_show(Project $project): Response
+    {
+        return $this->render('backend/project/show.html.twig', ['project' => $project,'tag'=>'show']);
+    }
+
+    /**
+     * @Route("backend/project/{id}/edit", name="project_edit", methods="GET|POST")
+     */
+    public function p_edit(Request $request, Project $project): Response
+    {
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('project_edit', ['id' => $project->getId()]);
+        }
+
+        return $this->render('backend/project/edit.html.twig', [
+            'project' => $project,
+            'tag'=>'editing',
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("backend/project/{id}", name="project_delete", methods="DELETE")
+     */
+    public function p_delete(Request $request, Project $project): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$project->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($project);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('project_index');
     }
 }
--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -6,17 +6,37 @@ use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ProjectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('devDate')
-            ->add('technologyUsed')
-            ->add('state')
-            ->add('body')
+            //->add()
+            ->add('title', TextType::class, array(
+                'attr' => ['class'=>'form-control'],
+            ))
+            ->add('devDate', DateType::class ,array(
+                'widget' => 'single_text',
+
+                //prevent rendering it as type =date to avoide HTML5
+                'html5' => false,
+
+                // i added a class that can be selected in JS
+                'attr' => ['class' => 'js-datepicker form-control'],
+                
+            ))
+            ->add('technologyUsed',null, array(
+                'attr' => ['class'=>'form-control'],
+            ))
+            ->add('state',null, array(
+                'attr' => ['class'=>'form-control'],
+            ))
+            ->add('body',null, array(
+                'attr' => ['class'=>'form-control'],
+            ))
         ;
     }
 
